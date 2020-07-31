9917c37fe8452497f2f95bef0e251a3d1b67bfc7
hotaery <626910647@qq.com>
2020-07-31 18:38:14 +0800

    delete

--- src/filesystem/file.h
+++
@@ -1,8 +0,0 @@
-#ifndef FILESYSTEM_FILE_H
-#define FILESYSTEM_FILE_H
-
-namespace filesystem {
-
-} // namespace filesystem
-
-#endif // FILESYSTEM_FILE_H
--- src/filesystem/file_path.h
+++
@@ -1,8 +0,0 @@
-#ifndef FILESYSTEM_FILE_PATH_H
-#define FILESYSTEM_FILE_PATH_H
-
-namespace filesystem {
-
-} // namespace filesystem
-
-#endif // FILESYSTEM_FILE_PATH_H 
